230ee3ebed01b3430e01753c6583bc929353f8c5
Rory A <rory.aitchison1@nhs.net>
2025-07-09 15:22:31 +0100

    feat: prefer env vars to passed in vars
    - MARKERS_TO_USE is modified so use is essential.
    - BASE_URL could be set elsewhere

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,4 @@ MARKERS_TO_USE=${2:=${MARKERS_TO_USE}}
 MARKERS_TO_USE="${MARKERS_TO_USE//-/ }"
 export COGNITO_USER_PASSWORD=${3:=${COGNITO_USER_PASSWORD}}
 
-pytest --tracing retain-on-failure --base-url $1 -m "$2"
+pytest --tracing retain-on-failure -p no:warnings --base-url $BASE_URL -m "$MARKERS_TO_USE"
