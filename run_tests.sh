03a27b4cbfeca5374364d4576822a7be73b3da4c
Rory A <rory.aitchison1@nhs.net>
2025-07-10 12:09:43 +0100

    feat: add comment explaining regex function

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MARKERS_TO_USE="${2//-/ }"
+MARKERS_TO_USE="${2//-/ }" # replace hyphens with spaces
 export COGNITO_USER_PASSWORD=$3
 
 pytest --tracing retain-on-failure --base-url $1 -m "$MARKERS_TO_USE"
