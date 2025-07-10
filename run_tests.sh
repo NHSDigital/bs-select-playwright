bc05275ad09ca1960cd615e473c475bd21933fc3
Rory A <rory.aitchison1@nhs.net>
2025-07-10 13:12:44 +0100

    refactor: increase readability and expressiveness

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+BASE_URL=$1
 MARKERS_TO_USE="${2//-/ }" # replace hyphens with spaces
 export COGNITO_USER_PASSWORD=$3
 
-pytest --tracing retain-on-failure --base-url $1 -m "$MARKERS_TO_USE"
+pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
