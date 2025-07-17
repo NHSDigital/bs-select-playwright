c1cf69e9cb7997a19fbcd03f86b95f44016fe283
Rory A <rory.aitchison1@nhs.net>
2025-07-17 16:55:42 +0100

    feat: reintroduce hyphen in jira ticket number

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 
 BASE_URL=$1
 MARKERS_TO_USE="${2//-/ }" # replace hyphens with spaces
+MARKERS_TO_USE=$(echo "$MARKERS_TO_USE" | sed -E 's/\b([A-Z]+) ([0-9]{4})\b/\1-\2/') # replace space in Jira ticket number(s) with hyphen
 export COGNITO_USER_PASSWORD=$3
 
 pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
