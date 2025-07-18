9494da2a4b912c2cb27bfd25f3e584f46f93b64b
Rory A <rory.aitchison1@nhs.net>
2025-07-18 13:24:19 +0100

    debug: add logging

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,8 @@
 BASE_URL=$1
 MARKERS_TO_USE="${2//-/ }" # replace hyphens with spaces
 MARKERS_TO_USE=$(echo "$MARKERS_TO_USE" | sed -E 's/\b([A-Z]+) ([0-9]{4})\b/\1-\2/') # replace space in Jira ticket number(s) with hyphen
+echo "here"
+echo $MARKERS_TO_USE
 export COGNITO_USER_PASSWORD=$3
 
 pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
