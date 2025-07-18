0387b84ca2287cb934194bdfdc8b33419d002597
Rory A <rory.aitchison1@nhs.net>
2025-07-18 16:27:53 +0100

    feat: received arg now uses commas instead of hyphens
    - because the second arg (based on the Jira ticket number) has a hyphen the hyphen is no longer suitable as a separator because hyphens are replaced with spaces

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 BASE_URL=$1
-MARKERS_TO_USE="${2//-/ }" # replace hyphens with spaces
-MARKERS_TO_USE=$(echo "$MARKERS_TO_USE" | sed -E 's/\b([A-Z]+) ([0-9]{4})\b/\1-\2/') # replace space in Jira ticket number(s) with hyphen
-echo "here"
-echo $MARKERS_TO_USE
+MARKERS_TO_USE="${2//,/ }" # replace commas with spaces
 export COGNITO_USER_PASSWORD=$3
 
 pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
