8f3e4ca738a446e852db1ad50272b7f28a76799d
Rory A <rory.aitchison1@nhs.net>
2025-07-21 17:42:31 +0100

    feat: construct branch marker from passed in branch name
    - construction of eg. "branch(serial='bss2-2403')" was previously being done in bs-select-app which is a case of leaky abstraction. This change keeps knowledge of exactly how a marker should be constructed in this repo

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-BASE_URL=$1
-MARKERS_TO_USE="${2//./ }" # replace full stops with spaces
-export COGNITO_USER_PASSWORD=$3
+BASE_URL="$1"
+ALL_MARKERS="$2"
+export COGNITO_USER_PASSWORD="$3"
+
+BRANCH_NAME="${ALL_MARKERS##*.}" # taken from tail of ALL_MARKERS
+LEADING_MARKERS="${ALL_MARKERS:0:$((${#ALL_MARKERS} - ${#BRANCH_NAME}))}" # taken from head of ALL_MARKERS
+BRANCH_MARKER_TEMPLATE="branch(serial='%')"
+BRANCH_MARKER="${BRANCH_MARKER_TEMPLATE//%/$BRANCH_NAME}" # interpolate BRANCH_NAME in to BRANCH_MARKER_TEMPLATE at %
+COMBINED_MARKERS="$LEADING_MARKERS$BRANCH_MARKER"
+MARKERS_TO_USE="${COMBINED_MARKERS//./ }" # replace full stops with spaces
 
 pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
