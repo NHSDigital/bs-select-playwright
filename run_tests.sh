ccf3d73eb3f9559d16aee8d730161e857a9a3796
Rory A <rory.aitchison1@nhs.net>
2025-07-21 13:43:12 +0100

    feat: received arg now uses full stops instead of commas
    - commas can't be used in directory names or S3 bucket names, which are required when the markers are used in bs-select-app

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE_URL=$1
-MARKERS_TO_USE="${2//,/ }" # replace commas with spaces
+MARKERS_TO_USE="${2//./ }" # replace full stops with spaces
 export COGNITO_USER_PASSWORD=$3
 
 pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
