cddf8a4b36bc53cf9d6ffe0dbbe03d6cd8dcb082
Rory A <rory.aitchison1@nhs.net>
2025-07-24 16:10:50 +0100

    feat: update run script to match bss2-2403

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-BASE_URL=$1
-MARKERS_TO_USE="${2//-/ }" # replace hyphens with spaces
-export COGNITO_USER_PASSWORD=$3
+BASE_URL="$1"
+ALL_MARKERS="$2"
+export COGNITO_USER_PASSWORD="$3"
+
+BRANCH_NAME="${ALL_MARKERS%%.*}" # taken from head of ALL_MARKERS
+TRAILING_MARKERS="${ALL_MARKERS:$((${#BRANCH_NAME}))}" # everything in ALL_MARKERS that is not BRANCH_NAME
+BRANCH_MARKER_TEMPLATE="branch(serial='%')"
+BRANCH_MARKER="${BRANCH_MARKER_TEMPLATE//%/$BRANCH_NAME}" # interpolate BRANCH_NAME in to BRANCH_MARKER_TEMPLATE at %
+COMBINED_MARKERS="$BRANCH_MARKER$TRAILING_MARKERS"
+MARKERS_TO_USE="${COMBINED_MARKERS//./ }" # replace full stops with spaces
 
 pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
