d12e07dbf3d118926863f5263893c72fb97e7712
Rory A <rory.aitchison1@nhs.net>
2025-07-30 08:13:23 +0100

    BSS2-2403 run tests marked with branch specific markers (#25)
    ## What?
    
    Receive the branch name and insert this in to a marker template before
    passing it to pytest
    
    ## Why?
    
    Sometimes we want to run a specific set of tests when we are developing
    new features.
    
    ## How?
    
    The branch name is extracted from the front of the markers argument and
    inserted into the marker template: `branch(serial='<branch name>')`.
    This marker will run any tests that are marked as such:
    `@pytest.mark.branch(serial='<branch name>')`
    
    ## Notes
    
    - Full stops are now used instead of commas as the separator in the
    markers argument, eg. `bss2-2403.or.smoke`, because:
    1. Spaces present problems when passed as args so we have to use
    something else.
    2. They are safe characters in docker container file paths and S3 bucket
    paths; the markers arg is used as a directory name.
    3. They are not allowed characters in python method names and so can be
    safely removed without checks, as they won't be present in any of the
    bodies of the markers themselves.
    
    - [Corresponding merge
    request](https://gitlab.mgmt.texasplatform.uk/bs-select/bs-select-app/-/merge_requests/944)
    in `bs-select-app`
    - [Jira issue](https://nhsd-jira.digital.nhs.uk/browse/BSS2-2403)

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
-BASE_URL=$1
-MARKERS_TO_USE="${2//-/ }" # replace hyphens with spaces
-export COGNITO_USER_PASSWORD=$3
+BASE_URL="$1"
+ALL_MARKERS="$2"
+export COGNITO_USER_PASSWORD="$3"
 
-pytest --tracing retain-on-failure --base-url $BASE_URL -m "$MARKERS_TO_USE"
+replace_full_stops_with_spaces() {
+  local input_string="$1"
+  echo "${input_string//./ }"
+}
+
+prepare_markers_containing_branch_name() {
+  local all_markers="$1"
+  local branch_name="${all_markers%%.*}" # taken from head of ALL_MARKERS. Branch name must be first if present.
+  local trailing_markers="${all_markers:${#branch_name}}" # everything in ALL_MARKERS that is not branch_name
+  local branch_marker_template="branch(serial='%')"
+  local branch_marker="${branch_marker_template//%/$branch_name}" # interpolate branch_name in to branch_marker_template at %
+  local combined_markers="$branch_marker$trailing_markers"
+  echo "$(replace_full_stops_with_spaces "$combined_markers")"
+}
+
+# if there are no markers (all tests will run)
+if [[ -z "$ALL_MARKERS" ]]; then
+  MARKERS_TO_USE=""
+
+# if a hyphen is present then at least one of the markers is a branch name
+# e.g. "bss2-2402", "bss2-2402.or.smoke"
+elif [[ "$ALL_MARKERS" =~ \- ]]; then
+  MARKERS_TO_USE="$(prepare_markers_containing_branch_name "$ALL_MARKERS")"
+
+# if a full stop is present there are multiple markers separated by full stops
+# e.g. "smoke.or.release"
+elif [[ "$ALL_MARKERS" =~ \. ]]; then
+  MARKERS_TO_USE="$(replace_full_stops_with_spaces "$ALL_MARKERS")"
+
+# a single non-branch name marker is present
+# e.g. "smoke"
+else
+  MARKERS_TO_USE=$ALL_MARKERS
+fi
+
+pytest --tracing retain-on-failure --base-url "$BASE_URL" -m "$MARKERS_TO_USE"
